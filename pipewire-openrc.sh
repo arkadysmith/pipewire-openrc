e04348893dc9dcb1f5f95dd3aa2a98e394266a48
arkadysmith <nw2025@protonmail.com>
2026-04-11 11:11:40 +0000

    Update pipewire-openrc.sh

--- pipewire-openrc.sh
+++ pipewire-openrc.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-set -e
+##pipewire setup for openrc
 
 cd ~/
-sudo pacman -Syu pipewire pipewire-pulse wireplumber &&
-sudo pacman -Syu pipewire-openrc pipewire-pulse-openrc wireplumber-openrc &&
+sudo pacman -S pipewire pipewire-pulse wireplumber &&
+sudo pacman -S pipewire-openrc pipewire-pulse-openrc wireplumber-openrc &&
 
 #Setup
 
+rc-update add -U pipewire default &
+rc-update add -U pipewire-pulse default &
+rc-update add -U woreplumber default
+
+##alternative way if above doesn't work
 ##create runlevel by user ID (e.g. default UID is 1000, can check it bay "echo $UID")----
 
-sudo mkdir /etc/runlevels/1000 &&
+#sudo mkdir /etc/runlevels/1000 &&
 
 ##Stack the run level----
 
-sudo rc-update -s add default 1000 &&
+#sudo rc-update -s add default 1000 &&
 
 ##Add service to user runlevel----
 
-sudo rc-update add pipewire 1000 &&
-
-sudo rc-update add pipewire-pulse 1000 &&
-
-sudo rc-update add wireplumber 1000 &&
+#sudo rc-update add pipewire 1000 &&
+#sudo rc-update add pipewire-pulse 1000 &&
+#sudo rc-update add wireplumber 1000 &&
 
 ##Append PAM to change runlevel after login----
 
 #echo "session optional pam_exec.so /usr/local/bin/user-service-start" >> /etc/pam.d/system-local-login
-
-#rc-update add -U pipewire default
-#rc-update add -U pipewire-pulse default
-#rc-update add -U woreplumber default
